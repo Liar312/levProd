3a8a0e9c61bcb236fed66263d9c89483a248d229
Liar123 <krasn20161@mail.ru>
2024-10-29 22:48:17 +0300

    настроил взаимодействие с сервисом редис

---
+++ src/main/java/com/example/testproject/Redis/MessagePublisher.java
@@ -0,0 +1,5 @@
+package com.example.testproject.Redis;
+
+public interface MessagePublisher {
+    void publish(final String message);
+}
--- src/main/java/com/example/testproject/Rest/UserRestController.java
+++ src/main/java/com/example/testproject/Rest/UserRestController.java
@@ -5,9 +5,11 @@ import com.example.testproject.Models.PlayerNameDto;
 import com.example.testproject.Services.PlayerService;
 import com.mysql.cj.log.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,10 +25,18 @@ public class UserRestController {
     }
     @PostMapping("/add/users")
     public void addPlayer(@RequestBody PlayerNameDto playerNameDto){
-               playerService.addPlayerByDTO(playerNameDto);//работает
+        if(playerService.getByLogin(playerNameDto.getLogin())==null) {
+            playerService.addPlayerByDTO(playerNameDto);//работает
+        }
+        else{
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, "Пользователь с таким логином уже существует"
+            );
+        }
     }
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+
         playerService.deletePlayerById(id);
         return ResponseEntity.ok("Пользователь удалён успешно");
     }
