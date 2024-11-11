375548e297f019d5b319d55d403ff8f97497778e
Liar123 <krasn20161@mail.ru>
2024-11-11 07:43:58 +0300

    отктил настройке без CassandraConfig.java

--- src/main/java/com/example/dnd_log_microservice/LogRepository/LogRepository.java
+++ src/main/java/com/example/dnd_log_microservice/LogRepository/LogRepository.java
@@ -2,10 +2,10 @@ package com.example.dnd_log_microservice.LogRepository;
 
 
 import com.example.dnd_log_microservice.LogModels.LogEntry;
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface LogRepository extends CassandraRepository<LogEntry,String> {
+public interface LogRepository extends JpaRepository<LogEntry,String> {
 }
